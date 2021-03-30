80a4bfd87958b04d229edd8139bafd1a190ee2f8
jlorezz <florianbress@gmail.com>
2021-03-30 13:04:41 +0200

    Legal Notice WIP

---
+++ src/views/LegalNotice.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="legalnotice">
+		<h1>This is an about page</h1>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+	name: "LegalNotice",
+};
+</script>
